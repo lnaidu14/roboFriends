e1ee87a3134248ea6ef0cf37abdabdb4a0a9f07b
lnaidu14 <lalit.pratap.naidu@gmail.com>
2021-02-17 00:55:24 +0530

    added errorBoundary

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -2,17 +2,23 @@ import React from "react";
 import Card from "./Card";
 
 const CardList = ({ robots }) => {
-  const cardArray = robots.map((user, i) => {
-    return (
-      <Card
-        key={robots[i].id}
-        id={robots[i].id}
-        name={robots[i].name}
-        email={robots[i].email}
-      />
-    );
-  });
-  return <div>{cardArray}</div>;
+  if (true) {
+    throw new Error("Nooooo!");
+  }
+  return (
+    <div>
+      {robots.map((user, i) => {
+        return (
+          <Card
+            key={robots[i].id}
+            id={robots[i].id}
+            name={robots[i].name}
+            email={robots[i].email}
+          />
+        );
+      })}
+    </div>
+  );
 };
 
 export default CardList;
